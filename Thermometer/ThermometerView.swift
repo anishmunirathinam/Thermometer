dbba70c2c772c6ce3f9f302232325154cc8b3fc1
Anish <anish@Anishs-Air.Dlink>
2019-08-06 23:13:29 +0530

    first commit

---
+++ Thermometer/ThermometerView.swift
@@ -0,0 +1,21 @@
+//
+//  ThermometerView.swift
+//  Thermometer
+//
+//  Created by Anish on 02/07/19.
+//  Copyright © 2019 Anish. All rights reserved.
+//
+
+import UIKit
+
+class ThermometerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
